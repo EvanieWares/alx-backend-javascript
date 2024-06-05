d24c54fed1d2a1bf0d7fb341eafb8a75de22e49b
EvanieWares <evaniewares@gmail.com>
2024-06-05 02:04:34 +0200

    Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,40 +1,31 @@
 const fs = require('fs');
 
 async function countStudents(path) {
-  if (!fs.existsSync(path)) {
-    throw new Error('Cannot load the database');
-  }
-
-  const students = await new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-      }
-      resolve(data.split('\n').map(student => student.split(',')));
+  if (fs.existsSync(path)) {
+    return new Promise((resolve) => {
+      fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+          throw Error('Cannot load the database');
+        }
+        const result = [];
+        data.split('\n').forEach((data) => {
+          result.push(data.split(','));
+        });
+        result.shift();
+        const newis = [];
+        result.forEach((data) => newis.push([data[0], data[3]]));
+        const fields = new Set();
+        newis.forEach((item) => fields.add(item[1]));
+        const final = {};
+        fields.forEach((data) => { (final[data] = 0); });
+        newis.forEach((data) => { (final[data[1]] += 1); });
+        console.log(`Number of students: ${result.filter((check) => check.length > 3).length}`);
+        Object.keys(final).forEach((data) => console.log(`Number of students in ${data}: ${final[data]}. List: ${newis.filter((n) => n[1] === data).map((n) => n[0]).join(', ')}`));
+        resolve(result, final, newis);
+      });
     });
-  });
-
-  students.shift();
-
-  const studentData = students.reduce((acc, student) => {
-    const [id, name, ...otherData] = student;
-    const major = otherData[otherData.length - 1];
-    acc[major] = acc[major] || { count: 0, students: [] };
-    acc[major].count++;
-    acc[major].students.push(name);
-    return acc;
-  }, {});
-
-  const totalStudents = students.filter(student => student.length > 3).length;
-
-  console.log(`Number of students: ${totalStudents}`);
-
-  Object.keys(studentData).forEach(major => {
-    const { count, students } = studentData[major];
-    console.log(`Number of students in ${major}: ${count}. List: ${students.join(', ')}`);
-  });
-
-  return [students, studentData, totalStudents];
+  }
+  throw Error('Cannot load the database');
 }
 
 module.exports = countStudents;
