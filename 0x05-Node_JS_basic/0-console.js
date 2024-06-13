3ce8e4d7d81befe2af7345a3524fbdce50b5fe22
EvanieWares <evaniewares@gmail.com>
2024-06-13 09:32:22 +0200

    Added docstring

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,10 @@
-const displayMessage = (msg) => console.log(msg);
+
+/**
+ * This function displays a message to the STDOUT.
+ *
+ * @param {String} msg - The message to be displayed
+ * @author Chisomo Psyelera <https://github.com/EvanieWares>
+ */
+const displayMessage = (msg) => process.stdout.write(msg + '\n');
 
 module.exports = displayMessage;
